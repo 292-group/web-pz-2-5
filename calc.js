75d254dfe4bc11ed81a37e2c6a0975c049ba6500
MyronKoza <myronkoza108@gmail.com.com>
2022-11-10 09:31:59 +0200

    mkoza  done

---
+++ calc.js
@@ -0,0 +1,160 @@
+let a='';
+ let b='';
+ let sign='';
+ let result='';
+ let finish=false;
+ const digit= ['0','1','2','3','4','5','6','7','8','9','.'];
+ const action=['-','+','×','÷'];
+ 
+
+ const out =document.querySelector('.output');
+ function clearAll () {
+    a= '';
+    b='';
+    sign='';
+    finish=false;
+    out.textContent=0; 
+ }
+
+ document.querySelector('.ac').onclick = clearAll;
+ 
+ document.querySelector('.all_btn').onclick=(event)=>{
+    if(!event.target.classList.contains('btn')) return;
+    if(event.target.classList.contains('ac')) return;
+    out.textContent='';
+    const key=event.target.textContent;
+
+    if(digit.includes(key)){
+      if(b==='' && sign ===''){
+        a+=key;
+        out.textContent=a;
+      }
+      else if(a!=='' && b!=='' &&finish)
+      {
+         b=key;
+         finish=false;
+         out.textContent=b;
+      }
+      else{
+         b+=key;
+         out.textContent=b;
+      }
+    }
+    if(action.includes(key)){
+      if(sign==''){
+      sign=key;
+      out.textContent=sign;
+   }
+   // console.log(`a=${a},sign=${sign},b=${b}`)
+
+      if(a!==''&&b!==''&&sign!==''){
+         switch(sign){
+            case '+':
+            a= (+a) + (+b) ;
+            
+            break;
+               case '-':
+                  a= a - b;
+                  break;
+                  case '×':
+                     a= a * b;
+                     break;
+                     case '+':
+                        a= a + b;
+                        break;
+                        case '÷':
+                           if(b==='0'){
+                              out.textContent="Помилочка:("
+                              a=''
+                              b=''
+                              sign=''
+                              return;
+                           }
+                           a= a / b;
+                           break;
+   }
+   console.log(`a=${a},sign=${sign},b=${b}`)
+   sign=key;
+   out.textContent=sign;
+
+   finish=true;
+       }
+      return;
+      
+    }
+    if(key==='='){
+      if(b==='') b=a 
+      else if(a!==''&&b!==''&&sign!==''){
+         switch(sign){
+            case '+':
+            a= (+a) + (+b) ;
+            
+            break;
+               case '-':
+                  a= a - b;
+                  break;
+                  case '×':
+                     a= a * b;
+                     break;
+                     case '+':
+                        a= a + b;
+                        break;
+                        case '÷':
+                           if(b==='0'){
+                              out.textContent="Помилочка:("
+                              a=''
+                              b=''
+                              sign=''
+                              return;
+                           }
+                           a= a / b;
+                           break;
+   }
+   console.log(`a=${a},sign=${sign},b=${b}`)
+   sign=key;
+   out.textContent=sign;
+
+   finish=true;
+       }
+    
+      out.textContent=a;
+    
+    }
+   
+    if(key==='+/-'){
+      if(a!=='0'&&sign===''){
+         a=-a;
+         out.textContent=a
+      }
+      else if(sign!=''&& b!=='0') {
+         b=-b
+         out.textContent=b
+      }
+    }
+    if (key === '%') {
+      b = a * (b / 100);
+      out.textContent = b;
+  }
+ }
+ const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
+const savebtn=document.querySelector('.save');
+const pastebtn=document.querySelector('.paste');
+
+savebtn.onclick=()=>{
+   result=a;
+   document.cookie=result;
+   console.log(document.cookie);
+}
+pastebtn.onclick=()=>{
+   out.textContent=document.cookie;
+   if(sign==='') a=document.cookie;
+   else if(a!=='' && sign!=='') b=document.cookie;
+}

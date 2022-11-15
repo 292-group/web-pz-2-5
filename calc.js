b971dbc8facdb01a1991f9491b8ba89f6d7c19ba
VovaStolyrhcuk <vova42n@gmail.com>
2022-11-15 10:05:51 +0200

    the theme changes to a red bottom

--- calc.js
+++ calc.js
@@ -92,9 +92,17 @@ document.querySelector('.buttons').onclick = (event) => {
     };
 };
 
-const btn = document.querySelector(".btn-toggle");
+
+
+
+
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
 let isDark = true;
-btn.onclick = () => {
-    btn.classList.toggle("dark");
-    isLight = !isDark;
-};
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+}
